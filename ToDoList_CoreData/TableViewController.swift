5a1710aaece61c46557276e1f62d43262bfca248
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-11 15:24:12 +0300

    feat: saving data using Core Data

--- ToDoList_CoreData/TableViewController.swift
+++ ToDoList_CoreData/TableViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
-    var tasks: [String] = []
-
+    var tasks: [Tasks] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func plusTasks(_ sender: UIBarButtonItem) {
@@ -23,7 +39,7 @@ class TableViewController: UITableViewController {
         let saveTask = UIAlertAction(title: "Save", style: .default) { action in
             let textField = alertController.textFields?.first
             if let newTask = textField?.text {
-                self.tasks.insert(newTask, at: 0)
+                self.saveTask(withTitle: newTask)
                 self.tableView.reloadData()
             }
         }
@@ -39,24 +55,66 @@ class TableViewController: UITableViewController {
         
     }
     
+    @IBAction func deleteTasks(_ sender: UIBarButtonItem) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        if let tasks = try? context.fetch(fetchRequest) {
+            for task in tasks {
+                context.delete(task)
+            }
+        }
+        
+        do {
+            try context.save()
+            
+        } catch let  error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func saveTask(withTitle title: String) {
+        //        добираемся до контекста
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Tasks", in: context) else { return }
+        
+        let taskObject = Tasks(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError{
+            print(error.localizedDescription)
+        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return tasks.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        cell.textLabel?.text = tasks[indexPath.row]
-
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        
         return cell
     }
-
+    
 }
